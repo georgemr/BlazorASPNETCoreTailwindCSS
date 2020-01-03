280f4e313652222d7fc6091bf7aa462c457c5325
Georgios Mourkousis <georgemr@infognosis.gr>
2020-01-04 01:33:41 +0200

    Create basic tailwind css design and set css watch task with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
-﻿module.exports = function (grunt) {
+﻿/// <binding ProjectOpened='watch-css' />
+module.exports = function (grunt) {
 
     grunt.initConfig({
 
@@ -24,79 +25,31 @@
                 dest: 'wwwroot/css/',
                 ext: '.css'
             }
+        },
+
+        // Watch for changes and run Tasks
+        watch: {
+            postcss: {
+                files: 'src/css/**/*.css',
+                tasks: ['compile-css'],
+                options: {
+                    interrupt: true
+                }
+            }
         }
 
     });
 
     // Load Grunt Plugins
     grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks("grunt-postcss");
+
+    // Register Tasks
+    grunt.registerTask('compile-css', ['clean', 'postcss']);
+
+    // Resgiter Watcher Tasks
+    grunt.registerTask('watch-css', ['watch:postcss']);
 
 };
 
-
-
-
-
-
-//module.exports = function (grunt) {
-//    grunt.initConfig({
-//        // get the configuration info from package.json
-//        pkg: grunt.file.readJSON('package.json'),
-
-//        // PostCSS - Tailwindcss and Autoprefixer
-//        postcss: {
-//            options: {
-//                map: true, // inline sourcemaps
-//                processors: [
-//                    require('tailwindcss')(),
-//                    require('autoprefixer')({ browsers: 'last 2 versions' }) // add vendor prefixes
-//                ]
-//            },
-//            dist: {
-//                expand: true,
-//                cwd: 'ui/src/tailwindcss/',
-//                src: ['**/*.css'],
-//                dest: 'ui/dist/tailwindcss/',
-//                ext: '.css'
-//            }
-//        },
-
-        //concat: {
-        //    all: {
-        //        src: ['src/css/*.css', 'src/css/tailwind/dist/*.css'],
-        //        dest: 'src/temp/app.css'
-        //    }
-        //},
-
-        //uglify: {
-        //    all: {
-        //        src: ['src/temp/app.css'],
-        //        dest: 'wwwroot/css/app.min.css'
-        //    }
-        //}
-
-//        // Watch for changes and run Tasks
-//        watch: {
-//            postcss: {
-//                files: 'ui/src/tailwindcss/**/*.css',
-//                tasks: ['compile-tailwindcss'],
-//                options: {
-//                    interrupt: true
-//                }
-//            }
-//        }
-//    })
-
-//    // Load Grunt Plugins
-//    grunt.loadNpmTasks('grunt-contrib-watch')
-//    grunt.loadNpmTasks('grunt-postcss')
-//    grunt.loadNpmTasks('grunt-contrib-concat');
-//    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-//    // Register Tasks
-//    grunt.registerTask('compile-tailwindcss', ['postcss'])
-
-//    // Resgiter Watcher Tasks
-//    grunt.registerTask('watch-tailwindcss', ['watch:postcss'])
-//}
